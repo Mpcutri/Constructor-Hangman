5830668b39a8c9794155bdfc7e342eeeb0a70074
Maxwell Cutri <mpcutri@gmail.com>
2018-02-20 13:53:23 -0700

    functioning game, needs more constructors

--- Letter.js
+++ Letter.js
@@ -1,15 +1,8 @@
 var inquirer = require("inquirer");
 
-//==================== ATTENTION: NOT YET COMPLETE. . WORKING THROUGH IT TODAY!! =========================//
-
 function Letter(name) {
   this.name = name;
   // creates the printInfo method and applies it to all programmer objects
-  this.printInfo = function() { // COMMENT THIS OUT! DOESN'T WORK. . . SHOULD I PASTE checkLetters() in here??
-    if (this.name ===)
-    console.log("Letter Guessed1: " + this.name);
-    console.log("====================");
-  };
 }
 
 // Array of Word Options (all lowercase)
@@ -24,16 +17,18 @@ var numBlanks = 0;
 // Holds a mix of blank and solved letters (ex: 'n, _ _, n, _').
 var blanksAndSuccesses = [];
 // Holds all of the wrong guesses
-var wrongGuesses = [];
+var wrongGuesses = "";
 // Game counters
 var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
+var displayWord = "";
 
 
 function startGame() {
   // Reset the guesses back to 0.
   numGuesses = 9;
+  console.log("==========\n" + "Wins: " + winCounter + " | Losses: " + lossCounter);
   // Solution is chosen randomly from wordList.
   chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
   // The word is broken into individual letters.
@@ -52,21 +47,23 @@ function startGame() {
     blanksAndSuccesses.push("_");
   }
   // Print the initial blanks in console.
-  console.log(blanksAndSuccesses);
+  // console.log(blanksAndSuccesses);
   // Reprints the guessesLeft to 9
   console.log("\nYou have " +numGuesses+ " guesses left!");
   // Prints the blanks at the beginning of each round in the HTML
   console.log(blanksAndSuccesses.join(" "));
   // Clears the wrong guesses from the previous round
   console.log(wrongGuesses.join(" "));
+  guessALetter();
 }
 
 function checkLetters(letter) {
+  displayWord = "";
   // This boolean will be toggled based on whether or not a user letter is found anywhere in the word.
   var letterInWord = false;
   // Check if a letter exists inside the array at all.
   for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
+    if (chosenWord[i] === letter.name) {
       // If the letter exists then toggle this boolean to true. This will be used in the next step.
       letterInWord = true;
     }
@@ -76,40 +73,39 @@ function checkLetters(letter) {
     // Loop through the word.
     for (var j = 0; j < numBlanks; j++) {
       // Populate the blanksAndSuccesses with every instance of the letter.
-      if (chosenWord[j] === letter) {
+      if (chosenWord[j] === letter.name) {
         // Here we set the specific space in blanks and letter equal to the letter when there is a match.
-        blanksAndSuccesses[j] = letter;
+        blanksAndSuccesses[j] = letter.name;
       }
     }
     // Logging for testing.
-    console.log(blanksAndSuccesses);
+    for (var k = 0; k < blanksAndSuccesses.length; k++) {
+      displayWord += blanksAndSuccesses[k] + " ";
+    }
+    console.log(displayWord);
   }
   // If the letter doesn't exist at all...
   else {
     // ..then we add the letter to the list of wrong letters, and we subtract one of the guesses.
-    wrongGuesses.push(letter);
-    console.log("Wrong Guesses: " + wrongGuesses + ", ")
+    wrongGuesses += letter.name + ", ";
+    console.log("Wrong Guesses: ", wrongGuesses);
     numGuesses--;
   }
 }
 
 function roundComplete() {
   // First, log an initial status update in the console telling us how many wins, losses, and guesses are left.
-  console.log("WinCount: " + winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
-  console.log("\n==========\n");
 
-  console.log("\n" + numGuesses);
+  // console.log("\n" + numGuesses);
 
-  console.log("\n" + blanksAndSuccesses.join(" "));
+  // console.log("\n" + blanksAndSuccesses.join(" "));
 
-  console.log("\n" + wrongGuesses.join(" "));
+  // console.log("\n" + wrongGuesses.join(" "));
 
   if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
 
     winCounter++;
-    console.log("\n==========" + "\nYou win!" + "\n==========");
-
-    console.log(winCounter);
+    console.log("\n==========" + "\nYou win!");
     startGame();
   }
 
@@ -117,34 +113,31 @@ function roundComplete() {
 
     lossCounter++;
 
-     console.log("\n==========" + "\nYou lose!" + "\n==========");
-
-    console.log(lossCounter);
-    // Restart the game.
+    console.log("\n==========" + "\nYou lose!");
     startGame();
   }
 }
 startGame();
 
-  var guessALetter = function() {
-    if (lettersInChosenWord.toString() !== blanksAndSuccesses.toString()) {
-      // INQUIRER 
-      inquirer.prompt([
-        {
-          name: "name",
-          message: "Guess a letter!"
-        }
-      ]).then(function(answers) {
-
-        var letterGuessed = new Letter(answers.name);
-        
-        checkLetters(letterGuessed);
-        console.log("\nYou have " +numGuesses+ " guesses left!\n");
-        guessALetter();
-      });
-    } else if (numGuesses === 0 || lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
+  function guessALetter() {
+    if (numGuesses === 0 || lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
         roundComplete();
+      // INQUIRER 
+    } else if (lettersInChosenWord.toString() !== blanksAndSuccesses.toString()) {
+        inquirer.prompt([
+          {
+            name: "name",
+            message: "Guess a letter!"
+          }
+        ]).then(function(answers) {
+
+          var letterGuessed = new Letter(answers.name);
+          
+          checkLetters(letterGuessed);
+          console.log("\nYou have " +numGuesses+ " guesses left!\n");
+          guessALetter();
+        });
       }
   } 
 
-guessALetter();
+// guessALetter();
